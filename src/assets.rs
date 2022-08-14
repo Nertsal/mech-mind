58d0b738ed24cfdec5d95fc00fdf458633b25b35
Nertsal <sasha.kudasov04@gmail.com>
2022-08-14 12:48:34 +0300

    Load new assets

--- src/assets.rs
+++ src/assets.rs
@@ -6,7 +6,13 @@ pub use pixel_texture::*;
 
 #[derive(geng::Assets)]
 pub struct Assets {
+    pub enemies: EnemyAssets,
     pub mech: MechAssets,
+}
+
+#[derive(geng::Assets)]
+pub struct EnemyAssets {
+    pub blighter: BlighterEnemy,
 }
 
 #[derive(geng::Assets)]
@@ -14,6 +20,16 @@ pub struct MechAssets {
     pub artillery: ArtilleryMech,
     pub healer: HealerMech,
     pub tank: TankMech,
+}
+
+#[derive(geng::Assets)]
+pub struct BlighterEnemy {
+    #[asset(range = "1..=16", path = "attack/*.png")]
+    pub attack: Vec<PixelTexture>,
+    #[asset(range = "1..=10", path = "walk/*.png")]
+    pub walk: Vec<PixelTexture>,
+    pub idle: PixelTexture,
+    pub projectile: PixelTexture,
 }
 
 #[derive(geng::Assets)]
@@ -41,6 +57,8 @@ pub struct TankMech {
     #[asset(range = "1..=10", path = "walk/*.png")]
     pub walk: Vec<PixelTexture>,
     pub idle: PixelTexture,
+    pub hand: PixelTexture,
+    pub weapon: PixelTexture,
 }
 
 impl Assets {
