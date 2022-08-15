f45b8f60f4dec67d6cc8eb91497b4d9042101e51
Nertsal <sasha.kudasov04@gmail.com>
2022-08-15 21:04:27 +0300

    Death sound

--- src/logic/deaths.rs
+++ src/logic/deaths.rs
@@ -3,13 +3,22 @@ use super::*;
 impl Logic<'_> {
     pub fn process_deaths(&mut self) {
         // Units
+        let mut sounds = Vec::new();
         self.model.units.retain(|unit| {
             let alive = unit.health.is_alive();
             if !alive {
                 self.model.player_energy.change(Hp::new(10.0));
+                let sound = match unit.faction {
+                    Faction::Mech => self.model.assets.sound_design.mechs.death.clone(),
+                    Faction::Alien => self.model.assets.sound_design.enemies.death.clone(),
+                };
+                sounds.push(sound);
             }
             alive
         });
+        for sound in sounds {
+            self.model.play_sound(&sound);
+        }
 
         // Projectiles
         for projectile in &mut self.model.projectiles {
